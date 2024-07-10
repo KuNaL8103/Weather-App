00e27d653aba23d05301d2f3306e33ae83322841
KuNaL8103 <153227163+KuNaL8103@users.noreply.github.com>
2024-07-10 19:42:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,13 +3,12 @@ const search = document.querySelector('.search-box button');
 const weatherBox = document.querySelector('.weather-box');
 const weatherDetails = document.querySelector('.weather-details');
 const error404 = document.querySelector('.not-found');
-require('dotenv').config();
 search.addEventListener('click', () => {
-    const apiKey = process.env.API_KEY;
+    const APIKey = 'YOUR API KEY';
     const city = document.querySelector('.search-box input').value;
     if (city === '')
         return;
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`)
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${APIKey}`)
         .then(response => response.json())
         .then(json => {
             if (json.cod === '404') {
